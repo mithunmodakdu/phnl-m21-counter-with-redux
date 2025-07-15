d0a9363563bedbcf26745ea9e61bc46047761890
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-07-15 10:52:12 +0600

    21-7 Set Up a Redux Store. 21-8 Creating your first slice. 21-9 Write Actions in Redux.

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import {useDispatch } from "react-redux"
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+
+function App() {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () =>{
+    dispatch(increment());
+  };
+
+  const handleDecrement = () =>{
+    dispatch(decrement());
+  }
+
+  return (
+    <div>
+      <h1>Counter with Redux</h1>
+      <button>Increment</button>
+      <div>0</div>
+      <button>Decrement</button>
+    </div>
+  )
+}
+
+export default App
